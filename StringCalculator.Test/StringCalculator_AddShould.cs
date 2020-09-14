4b3c942d07a50effdb2930a671d478549e7c7787
Nick Guerra <nickg@maliciousgames.com>
2020-09-14 05:27:38 -0500

    handle new line separators

--- StringCalculator.Test/StringCalculator_AddShould.cs
+++ StringCalculator.Test/StringCalculator_AddShould.cs
@@ -20,7 +20,28 @@ namespace StringCalculator.Test
       [Theory]
       [InlineData(2, "1,1")]
       [InlineData(3, "1,2")]
+      [InlineData(13, "11,2")]
       public void ReturnSumOfNumbers_GivenTwoNumbers(int expected, string input)
+      {
+         int result = Calculator.Add(input);
+
+         Assert.Equal(expected, result);
+      }
+
+      [Theory]
+      [InlineData(11, "1,1,2,3,4")]
+      [InlineData(9, "1,2,2,2,2")]
+      [InlineData(36, "11,2,23")]
+      public void ReturnSumOfNumbers_GivenMoreThanTwoNumbers(int expected, string input)
+      {
+         int result = Calculator.Add(input);
+
+         Assert.Equal(expected, result);
+      }
+
+      [Theory]
+      [InlineData(36, "11\n2,23")]
+      public void ReturnSumOfNumbers_GivenNewLineSeparators(int expected, string input)
       {
          int result = Calculator.Add(input);
 
